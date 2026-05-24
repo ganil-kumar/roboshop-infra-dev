7e6cf43f15611f55bcc7888a099aa8d5b8c6be9f
ganil-kumar <g.anilkumar3852@gmail.com>
2026-05-24 13:38:29 +0530

    terraform

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z0178053H1TTSPYHM852"
+}
+
+variable "domain_name" {
+    default = "akviklabs.online"
+}
