add083fe20421610129971bdbb1fecc73574ec7d
ganil-kumar <g.anilkumar3852@gmail.com>
2026-05-31 12:49:33 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group_rule" "mongodb_catalogue" {
   to_port           = 27017
   protocol          = "tcp"
   # Where traffic is comming from
-  source_security_group_id = local.catalouge_sg_id
+  source_security_group_id = local.catalogue_sg_id
   security_group_id = local.mongodb_sg_id
 }
 
@@ -79,22 +79,22 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   security_group_id = local.backend_alb_sg_id
 }
 
-resource "aws_security_group_rule" "catalouge_bastion" {
+resource "aws_security_group_rule" "catalogue_bastion" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
   # Where traffic is comming from
   source_security_group_id = local.bastion_sg_id
-  security_group_id = local.catalouge_sg_id
+  security_group_id = local.catalogue_sg_id
 }
 
-resource "aws_security_group_rule" "catalouge_backend_alb" {
+resource "aws_security_group_rule" "catalogue_backend_alb" {
   type              = "ingress"
   from_port         = 8080
   to_port           = 8080
   protocol          = "tcp"
   # Where traffic is comming from
   source_security_group_id = local.backend_alb_sg_id
-  security_group_id = local.catalouge_sg_id
+  security_group_id = local.catalogue_sg_id
 }
